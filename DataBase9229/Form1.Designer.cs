a651fc7fb948a4da0fdc2628ce099a350278e380
Mech Jean <jessiernutshell@outlook.com>
2020-02-20 13:50:01 +0300

    Added "quitLabel" Changed "StartPosition" to 'CenterScreen' Sized "mainFormPanel" due the "mainForm" size

--- DataBase9229/Form1.Designer.cs
+++ DataBase9229/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DataBase9229
 {
-    partial class Form1
+    partial class mainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,43 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
+            this.mainFormPanel = new System.Windows.Forms.Panel();
+            this.quitLabel = new System.Windows.Forms.Label();
+            this.mainFormPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // mainFormPanel
             // 
-            this.panel1.Controls.Add(this.panel2);
-            this.panel1.Location = new System.Drawing.Point(-1, -3);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 454);
-            this.panel1.TabIndex = 0;
+            this.mainFormPanel.Controls.Add(this.quitLabel);
+            this.mainFormPanel.Location = new System.Drawing.Point(1, 2);
+            this.mainFormPanel.Margin = new System.Windows.Forms.Padding(2);
+            this.mainFormPanel.Name = "mainFormPanel";
+            this.mainFormPanel.Size = new System.Drawing.Size(598, 367);
+            this.mainFormPanel.TabIndex = 0;
             // 
-            // panel2
+            // quitLabel
             // 
-            this.panel2.Location = new System.Drawing.Point(219, 180);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 100);
-            this.panel2.TabIndex = 0;
+            this.quitLabel.AutoSize = true;
+            this.quitLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.quitLabel.Location = new System.Drawing.Point(568, 9);
+            this.quitLabel.Name = "quitLabel";
+            this.quitLabel.Size = new System.Drawing.Size(19, 18);
+            this.quitLabel.TabIndex = 0;
+            this.quitLabel.Text = "X";
+            this.quitLabel.Click += new System.EventHandler(this.quitLabel_Click);
             // 
-            // Form1
+            // mainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel1);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.mainFormPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainForm";
-            this.panel1.ResumeLayout(false);
+            this.mainFormPanel.ResumeLayout(false);
+            this.mainFormPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel mainFormPanel;
+        private System.Windows.Forms.Label quitLabel;
     }
 }
 
--- DataBase9229/Form1.cs
+++ DataBase9229/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace DataBase9229
 {
-    public partial class Form1 : Form
+    public partial class mainForm : Form
     {
-        public Form1()
+        public mainForm()
         {
             InitializeComponent();
+        }
+
+        private void quitLabel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
